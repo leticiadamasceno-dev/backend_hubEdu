eb9e0128fe7f8154a91fab5623e3add81f21e331
leticiadamascebo-dev <leticiadamasceno01@gmail.com>
2024-11-18 21:46:09 -0300

    merge com a branch de token

--- db/database.js
+++ db/database.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize('hubEdu', 'root', '', {
   host: 'localhost',
-  port: 3307,
+//  port: 3307,
   dialect: 'mysql',
   dialectOptions: {
     connectTimeout: 60000 // opcional, aumenta o tempo limite de conexão
--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
-const sequelize = require('./db/database');
 require('./models/usuario'); 
 
 const sequelize = require('./db/database')
