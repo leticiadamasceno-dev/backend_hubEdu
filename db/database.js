b74dec85ebb0d2387b3d8f712033e78be7ff1d07
Bowie <marketing@letscode.com.br>
2024-11-20 00:00:49 -0300

    configuração de migrations apartir das novas atualizações

--- db/database.js
+++ db/database.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize('hubEdu', 'root', '', {
   host: 'localhost',
-//  port: 3307,
+  port: 3307,
   dialect: 'mysql',
   logging: false, // Desativa os logs de queries SQL se tiver te incomodando
 });
